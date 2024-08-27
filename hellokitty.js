d0ff587df57643bbe84da40f9347a0be05adf301
kenny7755 <95519565+kenny7755@users.noreply.github.com>
2024-08-26 20:27:56 -0700

    Create hellokitty.js

---
+++ hellokitty.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  });
+  
